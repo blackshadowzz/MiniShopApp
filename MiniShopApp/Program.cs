51ac63956bebfc8e2a77a8a1f0ff75b4524e2958
Black Shadow <rymang66@gmail.com>
2025-06-05 18:56:54 +0700

    Add project files.

---
+++ MiniShopApp/Program.cs
@@ -0,0 +1,37 @@
+using MiniShopApp.Components;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+var builder = WebApplication.CreateBuilder(args);
+var token = builder.Configuration["7823822574:AAFm2SzyMoepbVa6kMTElqlsbgr8JSUkkM4"];
+var webappUrl = builder.Configuration["BotWebAppUrl"];
+
+
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ TelegramBots/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Xml.Linq;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+
+internal class Program
+{
+    private static readonly string Token = "7823822574:AAFm2SzyMoepbVa6kMTElqlsbgr8JSUkkM4";
+    private static TelegramBotClient botClient;
+    private static void Main(string[] args)
+    {
+        TelegramBotClient botClient;
+
+
+
+        botClient = new TelegramBotClient(Token);
+        var result = botClient.GetMe().Result;
+
+        botClient.StartReceiving(UpdateHandler, ErrorHandler);
+
+}
+private static async Task UpdateHandler(ITelegramBotClient bot, Update update, CancellationToken token)
+        {
+
+            if (update.Type != UpdateType.Message)
+                return;
+            if (update.Message!.Type != MessageType.Text)
+                return;
+            switch (update.Message.Text.ToLower())
+            {
+                case "/start":
+                    var m = await
+                    botClient.SendMessage(
+                        chatId: update.Message.Chat,
+                        text: "Welcome to the bot! Type /help to see available commands."
+                    );
+                
+                    //txtMessage.Text = m.Chat.Username;
+                    //txtName.Text = m.Chat.FirstName + " " + m.Chat.LastName;
+                    break;
+
+                case "/help":
+                    await
+                    botClient.SendMessage(
+                        chatId: update.Message.Chat,
+                        text: "/start - Start the bot\n/help - Get help"
+                    );
+                    break;
+
+                default:
+                    await
+                    botClient.SendMessage(
+                        chatId: update.Message.Chat,
+                        text: "You said:\n" + update.Message.Text
+                    );
+                    break;
+            }
+        }
+        private static Task ErrorHandler(ITelegramBotClient bot, Exception exception, CancellationToken token)
+        {
+            var errorMessage = exception switch
+            {
+                ApiRequestException apiRequestException => $"Error : \n {apiRequestException.ErrorCode} \n {apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            return Task.CompletedTask;
+
+        }
+    }
