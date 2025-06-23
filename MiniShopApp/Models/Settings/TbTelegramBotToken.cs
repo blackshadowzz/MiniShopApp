98d6a448ccffebf868d3730175ccbee8ffc30c2d
Black Shadow <rymang66@gmail.com>
2025-06-24 06:52:58 +0700

    updated

--- MiniShopApp/Models/Settings/TbTelegramBotToken.cs
+++ MiniShopApp/Models/Settings/TbTelegramBotToken.cs
@@ -1,10 +1,14 @@
 ﻿namespace MiniShopApp.Models.Settings
 {
-    public class TbTelegramBotToken :BaseHelpEntity
+    public sealed class TbTelegramBotToken :BaseHelpEntity
     {
         //use to store back end Token and fix userId
         public int Id { get; set; }
-        public int UserId { get; set; }
+        public int? UserId { get; set; }
+        public int? UserType { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
         public string? BotToken { get; set; } = string.Empty;
         public string? BotTokenSecret { get; set;} = string.Empty;
     }
